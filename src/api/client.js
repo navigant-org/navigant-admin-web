535fc269f086c74b7d9e1b59e728cdce3a0a68a5
Shijin <shijinabraham2003@gmail.com>
2026-01-04 20:50:20 +0530

    Changed BaseUrl

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://navigant-backend.onrender.com/api';
+const API_BASE_URL = 'https://navigant-backend.azurewebsites.net/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
